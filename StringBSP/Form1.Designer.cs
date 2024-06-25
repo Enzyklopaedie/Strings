62524ce71e3b80bc642a70717ffdd7880da43ceb
Enzyklopaedie <122405388+Enzyklopaedie@users.noreply.github.com>
2024-06-25 11:27:30 +0200

    Add files via upload

---
+++ StringBSP/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace StringBSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 123);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 68);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Dein Name";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(62, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Namen eingeben";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(62, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 38);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Namenslänge bestimmen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 266);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(210, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(62, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 38);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Doppelnamen prüfen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(62, 359);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(210, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(62, 403);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(210, 38);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Vokale prüfen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(62, 447);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(210, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(62, 490);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(210, 38);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Namen drehen";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(62, 534);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(210, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(62, 576);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(210, 38);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Name ohne Umlaute";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(62, 620);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(210, 20);
+            this.textBox7.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 655);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ StringBSP/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace StringBSP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            textBox2.Text = "Hallo " + name + ",\r\nschön dich zu sehen. Heute schon Latein gelernt??";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            int nameslaenge = name.Length;
+            textBox3.Text = "Dein Name besteht aus " + nameslaenge.ToString() + " Zeichen.";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            char zeichen;
+            for (int i = 0; i < name.Length; i++)
+            {
+                zeichen = name[i];
+                if ((zeichen == '-') || (zeichen == ' '))
+                {
+                    textBox4.Text = name.ToString() + " ist ein Doppelname.";
+                    break;
+                }
+                else
+                {
+                    textBox4.Text = name.ToString() + " ist kein Dopplename.";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            char zeichen;
+            int counter = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                zeichen = name[i];
+                if ((zeichen == 'a') || (zeichen == 'e') || (zeichen == 'i') || (zeichen == 'o') || (zeichen == 'u') || (zeichen == 'ä') || (zeichen == 'ö') || (zeichen == 'ü'))
+                {
+                    counter ++;
+                }
+            }
+            textBox5.Text = name.ToString() + " hat " + counter.ToString() + " Vokale.";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            int length = name.Length;
+            int[] turned = new int[length];
+            char character;
+            string text = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                character = name[length - (i + 1)];
+                text = text + character;
+                textBox6.Text = text;
+            }            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string ausgabe = name;
+
+            ausgabe = ausgabe.Replace("ä", "ae");
+            ausgabe = ausgabe.Replace("ö", "oe");
+            ausgabe = ausgabe.Replace("ü", "ue");
+            ausgabe = ausgabe.Replace("Ä", "Ae");
+            ausgabe = ausgabe.Replace("Ö", "Oe");
+            ausgabe = ausgabe.Replace("Ü", "Ue");
+            ausgabe = ausgabe.Replace("ß", "ss");
+            ausgabe = ausgabe.Replace("ẞ", "SS");
+            textBox7.Text = ausgabe;
+            
+        }
+    }
+}
